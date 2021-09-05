93c89d9df698296fec3d121d01f4c0d669fe23cd
jenniekibiri <josphatje@gmail.com>
2021-09-05 15:29:56 +0300

    mongo con

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
+const { constants } = require('buffer');
 const express = require('express')
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const cors = require('cors')
+const graphl=require('express-graphql')
+mongoose.connect(url)
+
 const app = express();
 const port=5000
 app.listen(port,console.log(`server running on port ${port}`))
