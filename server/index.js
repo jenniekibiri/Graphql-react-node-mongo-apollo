4ec17ad423513cb350d92daccbd480f44eff096a
jenniekibiri <josphatje@gmail.com>
2021-09-05 15:34:05 +0300

    db config

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
-const { constants } = require('buffer');
-const express = require('express')
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const cors = require('cors')
-const graphl=require('express-graphql')
-mongoose.connect(url)
+const { constants } = require("buffer");
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+const graphl = require("express-graphql");
+const dotenv = require("dotenv").config();
+mongoose.connect(process.env.TESTDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("db connected");
+});
 
 const app = express();
-const port=5000
-app.listen(port,console.log(`server running on port ${port}`))
+const port = 5000;
+app.listen(port, console.log(`server running on port ${port}`));
